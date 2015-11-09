ab3f3d9e992a66d07f87202d84f776ae4590e27a
Pradeep Mahale <pradymm@gmail.com>
2015-11-08 20:33:18 -0500

    Initial commit

--- src/main/test/com/neustar/controller/FileReaderControllerTest.java
+++ src/main/test/com/neustar/controller/FileReaderControllerTest.java
@@ -1,14 +1,52 @@
 package com.neustar.controller;
 
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestContext;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {TestContext.class})
-@WebAppConfiguration
-public class FileReaderControllerTest  {
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.neustar.model.FileContent;
+import com.neustar.model.FileDetails;
+import com.neustar.service.FileReaderServiceImpl;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = {TestContext.class})
+//@WebAppConfiguration
+public class FileReaderControllerTest {
+
+	@Autowired
+	FileReaderServiceImpl fileReader;
+
+	@Test
+	public void testReadFileContentService() throws Exception {
+
+		FileReaderServiceImpl fileReaderService = Mockito.mock(FileReaderServiceImpl.class);
+		String s = "samplethree.txt";
+
+		ArrayList<FileContent> fileContents1 = new ArrayList<FileContent>();
+		FileContent f1 = new FileContent();
+		f1.setFileContent("PERSON");
+		ArrayList<FileDetails> fileDetails = fileReaderService.readFileService(s);
+		ArrayList<FileContent> fileContents2 = new ArrayList<FileContent>();
+		fileContents1.add(f1);
+		fileContents2.add(f1);
+		assertEquals(fileContents1, fileContents1);
+	}
+
+	@Test
+	public void testGetFileNames() throws Exception {
+
+		FileReaderServiceImpl fileReaderService = Mockito.mock(FileReaderServiceImpl.class);
+		String s = "samplethree.txt";
+
+		ArrayList<String> filename1 = new ArrayList<String>();
+		ArrayList<String> filename2 = new ArrayList<String>();
+		filename1.add(s);
+		filename2.add(s);
+		assertEquals(filename1, filename2);
+	}
 
 }
--- src/main/test/com/neustar/service/FileReaderServiceImplTest.java
+++ src/main/test/com/neustar/service/FileReaderServiceImplTest.java
@@ -1,5 +1,59 @@
 package com.neustar.service;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.neustar.dao.FileReaderDaoImpl;
+import com.neustar.model.FileContent;
+import com.neustar.model.FileDetails;
+
 public class FileReaderServiceImplTest {
 
+	@Autowired
+	FileReaderServiceImpl fileReader;
+
+	@Test
+	public void testReadFileService() throws Exception {
+
+		FileReaderServiceImpl fileReaderService = Mockito.mock(FileReaderServiceImpl.class);
+		String s = "samplethree.txt";
+
+		FileReaderDaoImpl fileReaderDao = Mockito.mock(FileReaderDaoImpl.class);
+
+		ArrayList<FileDetails> fileDetails1 = new ArrayList<FileDetails>();
+		FileDetails f1 = new FileDetails();
+		f1.setCategory("PERSON");
+		f1.setCount(1);
+		Set<String> fileLines = fileReaderDao.readFile(s);
+		ArrayList<FileDetails> fileDetails2 = new ArrayList<FileDetails>();
+		fileDetails2.add(f1);
+		fileLines.add("PRSON");
+		fileDetails1.add(f1);
+		assertEquals(fileDetails1, fileDetails2);
+	}
+
+	@Test
+	public void testReadFileContentService() throws Exception {
+
+		FileReaderServiceImpl fileReaderService = Mockito.mock(FileReaderServiceImpl.class);
+		String s = "samplethree.txt";
+
+		FileReaderDaoImpl fileReaderDao = Mockito.mock(FileReaderDaoImpl.class);
+
+		ArrayList<FileContent> fileContents1 = new ArrayList<FileContent>();
+		FileContent f1 = new FileContent();
+		f1.setFileContent("PERSON");
+		Set<String> fileLines = fileReaderDao.readFile(s);
+		ArrayList<FileContent> fileContents2 = new ArrayList<FileContent>();
+		fileContents1.add(f1);
+		fileLines.add("PRSON");
+		fileContents2.add(f1);
+		assertEquals(fileContents1, fileContents1);
+	}
 }
